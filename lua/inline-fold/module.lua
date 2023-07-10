bbc5c9c562548fb7f525bea568f5975c5fbb2df6
Mathis Albertzard <malbertzard@gmail.com>
2023-07-10 14:49:42 +0200

    feat: Rewritte using conceal. Breaks with changes in class. WIP

--- lua/inline-fold/module.lua
+++ lua/inline-fold/module.lua
@@ -2,42 +2,30 @@ local M = {}
 
 local hide = false
 local original_classes = {}
-local tick_values = {}
 
 local function find_class(line_content)
     local start_pos, end_pos, class_value = string.find(line_content, 'class="([^"]*)"')
     return start_pos, end_pos, class_value
 end
 
-local function store_original_class(bufnr, line, class_value)
-    if not original_classes[bufnr] then
-        original_classes[bufnr] = {}
-    end
-    original_classes[bufnr][line] = class_value
-end
+local function create_conceal(text)
+    local syntax_group = "InlineFold" -- Name of the syntax group
+    local conceal_char = "*" -- Character to use for concealment
 
-local function restore_original_class(bufnr, line)
-    local original_class = original_classes[bufnr] and original_classes[bufnr][line]
-    if original_class then
-        original_classes[bufnr][line] = nil
-        return original_class
-    end
-    return nil
-end
+    -- Define the syntax group
+    vim.cmd("syntax match " .. syntax_group .. " /" .. text .. "/ conceal cchar=" .. conceal_char)
 
-local function generate_tag_id(bufnr, line)
-    return bufnr .. ":" .. line
+    -- Set the concealment options
+    vim.cmd("highlight! link " .. syntax_group .. " Conceal")
+
+    -- Refresh the buffer to apply the concealment
+    vim.api.nvim_buf_call(0, function()
+        vim.cmd("syntax sync fromstart")
+    end)
 end
 
 local function update_buffer(bufnr)
     local tick = vim.api.nvim_buf_get_changedtick(bufnr)
-
-    if tick_values[bufnr] and tick_values[bufnr] == tick then
-        -- No changes in the buffer, no need to update
-        return
-    end
-
-    tick_values[bufnr] = tick
 
     vim.api.nvim_buf_clear_namespace(bufnr, -1, 0, -1)
 
@@ -49,18 +37,9 @@ local function update_buffer(bufnr)
         local start_pos, end_pos, class_value = find_class(line_content)
 
         if start_pos and end_pos then
-            local tag_id = generate_tag_id(bufnr, line)
-
             if hide then
-                store_original_class(bufnr, tag_id, class_value)
-                local new_line = line_content:gsub('class="[^"]+"', 'class="..."')
-                vim.api.nvim_buf_set_lines(bufnr, line - 1, line, false, { new_line })
+                create_conceal(class_value)
             else
-                local original_class = restore_original_class(bufnr, tag_id)
-                if original_class then
-                    local new_line = line_content:gsub('class="..."', 'class="' .. original_class .. '"')
-                    vim.api.nvim_buf_set_lines(bufnr, line - 1, line, false, { new_line })
-                end
             end
         end
     end
@@ -70,20 +49,6 @@ function M.toggle_hide()
     hide = not hide
 
     local bufnr = vim.api.nvim_get_current_buf()
-    update_buffer(bufnr)
-end
-
-function M.restore_classes()
-    local bufnr = vim.api.nvim_get_current_buf()
-    
-    hide = false
-    update_buffer(bufnr)
-end
-
-function M.rehide_classes()
-    local bufnr = vim.api.nvim_get_current_buf()
-
-    hide = true
     update_buffer(bufnr)
 end
 
--- plugin/inline-fold.lua
+++ plugin/inline-fold.lua
@@ -1,3 +1,1 @@
 vim.api.nvim_create_user_command("InlineFoldToggle", require("inline-fold").toggle, {})
-vim.api.nvim_create_user_command("InlineFoldShow", require("inline-fold").show, {})
-vim.api.nvim_create_user_command("InlineFoldHide", require("inline-fold").hide, {})
