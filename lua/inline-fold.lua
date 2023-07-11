9ac46cf0e74be28ab8732471eab7798f665c81df
Mathis Albertzard <malbertzard@gmail.com>
2023-07-11 15:04:17 +0200

    feat: Added configurable placeholder

--- lua/inline-fold.lua
+++ lua/inline-fold.lua
@@ -8,14 +8,11 @@ M.config = {
 
 -- setup is the public method to setup your plugin
 M.setup = function(args)
-  -- you can define your setup function here. Usually configurations can be merged, accepting outside params and
-  -- you can also put some validation here for those.
-
   M.config = vim.tbl_deep_extend("force", M.config, args or {})
 end
 
 M.toggle = function()
-  module.toggle_hide()
+  module.toggle_hide(M.config)
 end
 
 return M
--- lua/inline-fold/module.lua
+++ lua/inline-fold/module.lua
@@ -7,9 +7,9 @@ local function find_class(line_content)
   return start_pos, end_pos, class_value
 end
 
-local function create_conceal(text)
+local function create_conceal(text, placeholder)
   local syntax_group = "InlineFold" -- Name of the syntax group
-  local conceal_char = "*" -- Character to use for concealment
+  local conceal_char = placeholder
 
   -- Define the syntax group
   vim.cmd("syntax match " .. syntax_group .. " /" .. text .. "/ conceal cchar=" .. conceal_char)
@@ -35,7 +35,7 @@ local function remove_conceal()
   end)
 end
 
-local function update_buffer(bufnr)
+local function update_buffer(bufnr, placeholder)
   vim.api.nvim_buf_clear_namespace(bufnr, -1, 0, -1)
 
   local line_count = vim.api.nvim_buf_line_count(bufnr)
@@ -47,7 +47,7 @@ local function update_buffer(bufnr)
 
     if start_pos and end_pos then
       if hide then
-        create_conceal(class_value)
+        create_conceal(class_value, placeholder)
       else
         remove_conceal()
       end
@@ -62,12 +62,12 @@ local function set_conceal_level(bufnr)
   end)
 end
 
-function M.toggle_hide()
+function M.toggle_hide(conf)
   hide = not hide
 
   local bufnr = vim.api.nvim_get_current_buf()
   set_conceal_level(bufnr)
-  update_buffer(bufnr)
+  update_buffer(bufnr, conf.placeholder)
 end
 
 return M
